8f8401296f0a1c140c9aea9c0ac8c30cfe2eaee4
Eklas-Mohin <mohin030299@gmail.com>
2025-01-22 15:56:25 +0600

    O(1) Solution - Kadane's Algorithm

---
+++ Maximum-Subarray-Sum/maximum_subarray_sum_3.py
@@ -0,0 +1,16 @@
+def main():
+    given_array = [-1, 2, 4, -3, 5, 7, -5, 6]
+    n = len(given_array)
+    maximum_subarray_sum = float('-inf')
+    cnt = 0
+
+    for i in range(n):
+        cnt += given_array[i]
+        cnt = max(cnt, given_array[i])
+        maximum_subarray_sum = max(cnt, maximum_subarray_sum)
+
+    print("Total number of subarrays:", (n * (n + 1)) // 2)
+    print("Maximum subarray sum:", maximum_subarray_sum)
+
+if __name__ == '__main__':
+    main()
